1906d63f769ee14fc47c00dde02e2ebc8f111dcf
baraa1999 <baraaalian7@gmail.com>
2023-09-12 12:12:59 +0300

    Create the Categories widget in the Home Screen

--- lib/consts/app_constants.dart
+++ lib/consts/app_constants.dart
@@ -1,3 +1,4 @@
+import 'package:shopsmart_users_ar/models/categories_model.dart';
 import 'package:shopsmart_users_ar/services/assets_manger.dart';
 
 class AppContants {
@@ -7,5 +8,47 @@ class AppContants {
   static List<String> bannersImages = [
     AssetsManager.banner1,
     AssetsManager.banner2
+  ];
+  static List<CategoryModel> categoriesList = [
+    CategoryModel(
+      id: "Phones",
+      image: AssetsManager.mobiles,
+      name: "Phones",
+    ),
+    CategoryModel(
+      id: "Laptops",
+      image: AssetsManager.pc,
+      name: "Laptops",
+    ),
+    CategoryModel(
+      id: "Electronics",
+      image: AssetsManager.electronics,
+      name: "Electronics",
+    ),
+    CategoryModel(
+      id: "Watches",
+      image: AssetsManager.watch,
+      name: "Watches",
+    ),
+    CategoryModel(
+      id: "Clothes",
+      image: AssetsManager.fashion,
+      name: "Clothes",
+    ),
+    CategoryModel(
+      id: "Shoes",
+      image: AssetsManager.shoes,
+      name: "Shoes",
+    ),
+    CategoryModel(
+      id: "Books",
+      image: AssetsManager.book,
+      name: "Books",
+    ),
+    CategoryModel(
+      id: "Cosmetics",
+      image: AssetsManager.cosmetics,
+      name: "Cosmetics",
+    ),
   ];
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:shopsmart_users_ar/consts/app_constants.dart';
 import 'package:shopsmart_users_ar/providers/theme_provider.dart';
 import 'package:shopsmart_users_ar/services/assets_manger.dart';
 import 'package:shopsmart_users_ar/widgets/app_name_text.dart';
+import 'package:shopsmart_users_ar/widgets/products/ctg_rounded_widget.dart';
 import 'package:shopsmart_users_ar/widgets/products/latest_arrival.dart';
 import 'package:shopsmart_users_ar/widgets/subtitle_text.dart';
 import 'package:shopsmart_users_ar/widgets/titile_text.dart';
@@ -32,54 +33,73 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                log('Todo navigate to the product details screen');
-              },
-              child: SizedBox(
-                height: size.height * 0.24,
-                child: ClipRRect(
-                  child: Swiper(
-                    itemBuilder: (BuildContext context, int index) {
-                      return Image.asset(
-                        AppContants.bannersImages[index],
-                        fit: BoxFit.fill,
-                      );
-                    },
-                    autoplay: true,
-                    itemCount: AppContants.bannersImages.length,
-                    pagination: const SwiperPagination(
-                        alignment: Alignment.bottomCenter,
-                        builder: DotSwiperPaginationBuilder(
-                            color: Colors.white, activeColor: Colors.red)),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  log('Todo navigate to the product details screen');
+                },
+                child: SizedBox(
+                  height: size.height * 0.24,
+                  child: ClipRRect(
+                    child: Swiper(
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.asset(
+                          AppContants.bannersImages[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      autoplay: true,
+                      itemCount: AppContants.bannersImages.length,
+                      pagination: const SwiperPagination(
+                          alignment: Alignment.bottomCenter,
+                          builder: DotSwiperPaginationBuilder(
+                              color: Colors.white, activeColor: Colors.red)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const TitlesTextWidget(
-              label: "Last arival",
-              fontSize: 22,
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            SizedBox(
-              height: size.height * 0.2,
-              child: ListView.builder(
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return const LatestArrivalProductWidget();
-                  }),
-            )
-          ],
+              const SizedBox(
+                height: 18,
+              ),
+              const TitlesTextWidget(
+                label: "Last arival",
+                fontSize: 22,
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              SizedBox(
+                height: size.height * 0.2,
+                child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return const LatestArrivalProductWidget();
+                    }),
+              ),
+              const TitlesTextWidget(
+                label: "Categories",
+                fontSize: 22,
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              GridView.count(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 4,
+                  children:
+                      List.generate(AppContants.categoriesList.length, (index) {
+                    return CategoryRoundedWidget(
+                        image: AppContants.categoriesList[index].image,
+                        name: AppContants.categoriesList[index].name);
+                  }))
+            ],
+          ),
         ),
       ),
     );
