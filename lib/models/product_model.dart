a5dc8b3120a1ee510e15af067bb01d13a8a0e706
baraa1999 <baraaalian7@gmail.com>
2023-09-16 17:12:42 +0300

    Create the Product Model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,17 @@
+class ProductModel {
+  String productId,
+      productTitle,
+      productPrice,
+      productCategory,
+      productDescription,
+      productImage,
+      productQuantity;
+  ProductModel(
+      {required this.productId,
+      required this.productTitle,
+      required this.productPrice,
+      required this.productCategory,
+      required this.productDescription,
+      required this.productImage,
+      required this.productQuantity});
+}
